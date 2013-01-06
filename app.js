3ac7f01ab793956744d38913521cfcbebdbeb3f8
Sebastian Troc <sebastian.troc@gucman.pl>
2013-01-06 02:03:43 +0100

    Instalacja drivera MongoDB inicjalizacja w aplikacji

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var app = require('express').createServer();
+var express = require('express')
+,		app 		= express.createServer()
+,		mongo 	= require('mongodb');
+
 app.get('/', function(req, res) {
     res.send('Działa!');
 });
+
 app.listen(process.env.VCAP_APP_PORT || 3000);
