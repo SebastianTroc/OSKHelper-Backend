b543d67802020c60789ec0fe3bd8977025e5b83c
Sebastian Troc <sebastian.troc@gucman.pl>
2013-01-06 15:51:08 +0100

    Struktura z generatora Express.js z uwzględnieniem poprzednich funkcjonalności, które podczas generowania zostały napisane

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express')
-,		app 		= express.createServer()
-,		mongo 	= require('mongodb')
-,		db 			= new mongo.Db('oskhelper', new mongo.Server( 'localhost', 27017, {} ) , {});
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  ,	mongo 	= require('mongodb')
+	,	db 			= new mongo.Db('oskhelper', new mongo.Server( 'localhost', 27017, {} ) , {});
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+/**
+ * MongoDB test.
+ */
 
 var dataFromMongoDB;
-
 // Node odpala ta funkcje, gdy mongo jest juz dostepne do uzycia
 db.open(function(){
 
@@ -34,10 +66,14 @@ db.open(function(){
 
 	});
 
-})
+});
 
-app.get('/', function(req, res) {
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/db_test', function(req, res) {
     res.send(dataFromMongoDB);
 });
 
-app.listen(process.env.VCAP_APP_PORT || 3000);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
