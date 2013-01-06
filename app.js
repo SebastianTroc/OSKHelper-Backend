1bd64e608e1e59ceacca647f8c17a5463d8c6a4f
Sebastian Troc <sebastian.troc@gucman.pl>
2013-01-06 02:04:54 +0100

    Połączenie z Mongo i inicjacja testowej kolekcji oraz wypełnienie jej testowymi danymi

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 var express = require('express')
 ,		app 		= express.createServer()
-,		mongo 	= require('mongodb');
+,		mongo 	= require('mongodb')
+,		db 			= new mongo.Db('oskhelper', new mongo.Server( 'localhost', 27017, {} ) , {});
+
+// Node odpala ta funkcje, gdy mongo jest juz dostepne do uzycia
+db.open(function(){
+
+	db.collection('testCollection', function(err, collection){
+
+		doc = {
+			"name" : "MongoDB",
+			"type" : "database",
+			"count": 1,
+			"info" : {
+				x: 203,
+				y: 102
+			}
+		};
+
+		collection.insert(doc, function(){
+			console.log("wstawilem dane do testowej kolekcji 'testCollection'");
+		})
+
+	});
+
+})
 
 app.get('/', function(req, res) {
     res.send('Działa!');
